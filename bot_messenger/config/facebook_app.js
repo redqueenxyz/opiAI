a799394abfe99b1639f7edef1493b9b28d1073cb
Vivek Menon <mail@vvkmnn.xyz>
2017-05-29 14:55:54 -0400

    This needs to be updated once CI/CD is done.

--- bot_messenger/config/facebook_app.js
+++ bot_messenger/config/facebook_app.js
@@ -1,5 +1,6 @@
 module.exports = {
   appID: "719772424894824",
   appSecret: "037ac938ebc7e353d0bba9ede237260b",
-  accessToken: "719772424894824|037ac938ebc7e353d0bba9ede237260b"
+  accessToken: "719772424894824|037ac938ebc7e353d0bba9ede237260b",
+  callbackUrl: ""
 };
