5192d43427eb6e76dc8d5ff0403165f0ae02d6e5
Vivek Menon <mail@vvkmnn.xyz>
2017-05-30 14:21:31 -0400

    Prepping to switch to Shippable.

---
+++ tests/test.js
@@ -0,0 +1,10 @@
+// TODO: Incorporate Mocha and Chai - https://semaphoreci.com/community/tutorials/getting-started-with-node-js-and-mocha
+
+var assert = require('assert');
+describe('Array', function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+    });
+});
