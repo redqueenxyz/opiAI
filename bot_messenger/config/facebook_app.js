6915b1bbe23b44c7b7d0d04381985324d6dc7672
Vivek Menon <mail@vvkmnn.xyz>
2017-06-08 14:16:58 -0400

    Bye ngrok.

--- bot_messenger/config/facebook_app.js
+++ bot_messenger/config/facebook_app.js
@@ -2,5 +2,5 @@ module.exports = {
   appID: "719772424894824",
   appSecret: "037ac938ebc7e353d0bba9ede237260b",
   accessToken: "719772424894824|037ac938ebc7e353d0bba9ede237260b",
-  callbackUrl: ""
+  callbackUrl: "https://feedback-170115.appspot.com"
 };
