bb2bf42491948ebd0e5fdc54125345dd23d3cf97
Vivek Menon <mail@vvkmnn.xyz>
2017-08-08 14:36:28 -0400

    ECMAScript 6!

--- bot-messenger/routes/object_sender.js
+++ bot-messenger/routes/object_sender.js
@@ -1,150 +1,174 @@
 // Sends messages back to Facebook
-var exports = module.exports = {};
+let exports = module.exports = {};
 
 // Package Dependencies
-var request = require('request')
-var logger = require('winston')
+let request = require('request');
+let logger = require('winston');
 
 // Local Dependencies
-var facebook = require('../config/facebook');
+let facebook = require('../config/facebook');
 
 // Facebook Send API 
 /** This function interacts with the Facebook Send Api, so it is called with every message template, and handles actually submitting the final POST request to the Send API / Facebook Messenger */
-exports.callSendAPI = function (messageData) {
-
+exports.callSendAPI = function(messageData) {
   // Log
-  logger.info("...Preparing Object: ", { messageData })
+  logger.info('...Preparing Object: ', {messageData});
 
   request({
     uri: 'https://graph.facebook.com/v2.8/me/messages', // The API endpoint to POST to
-    qs: { access_token: facebook.pageAccessToken },
+    qs: {access_token: facebook.pageAccessToken},
     method: 'POST',
-    json: messageData // actual message to send to the Send API 
+    json: messageData, // actual message to send to the Send API 
 
-  }, function (error, response, body) {
+  }, function(error, response, body) {
     if (!error && response.statusCode == 200) {
-
       // If there's NO error or the response is good (200), then print the message
-      logger.warn(" Sending Successful! ", {
-        status: response.statusCode
-      })
-
+      logger.warn(' Sending Successful! ', {
+        status: response.statusCode,
+      });
     } else {
-      logger.warn(" Sending Error... ", {
-        error: error
-      })
+      logger.warn(' Sending Error... ', {
+        error: error,
+      });
     }
   });
-}
+};
 
 
 // TODO: Move these message templates.
 // Send Any Message
-exports.sendMessage = function (recipientId, messageObject) {  
+exports.sendMessage = function(recipientId, messageObject) {
   // Intialize the messageData object that FB will recieve
-  var messageData = {
+  let messageData = {
     recipient: {
-      id: recipientId
+      id: recipientId,
     },
-    message: messageObject
-  }
+    message: messageObject,
+  };
   exports.callSendAPI(messageData);
 };
 
 
 // Send Text Message 
-exports.sendTextMessage = function (recipientId, messageText) {
-
+exports.sendTextMessage = function(recipientId, messageText) {
   // Intialize the messageData object that FB will recieve
-  var messageData = {
+  let messageData = {
     recipient: {
-      id: recipientId
+      id: recipientId,
     },
     message: {
-      text: messageText
-    }
-  }
+      text: messageText,
+    },
+  };
   exports.callSendAPI(messageData);
 };
 
 // Send Templates
-exports.sendStructuredMessage = function (recipientId) {
+exports.sendStructuredMessage = function(recipientId) {
   console.log('\nWe heard \'generic\', get the Structured Message template!');
-  var messageData = {
+  let messageData = {
     recipient: {
-      id: recipientId
+      id: recipientId,
     },
     message: {
       attachment: {
-        type: "template",
+        type: 'template',
         payload: {
-          template_type: "generic",
+          template_type: 'generic',
           elements: [{
-            title: "rift",
-            subtitle: "Next-generation virtual reality",
-            item_url: "https://www.oculus.com/en-us/rift/",
-            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+            title: 'rift',
+            subtitle: 'Next-generation virtual reality',
+            item_url: 'https://www.oculus.com/en-us/rift/',
+            image_url: 'http://messengerdemo.parseapp.com/img/rift.png',
             buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/rift/",
-              title: "Open Web URL"
+              type: 'web_url',
+              url: 'https://www.oculus.com/en-us/rift/',
+              title: 'Open Web URL',
             }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for first bubble",
+              type: 'postback',
+              title: 'Call Postback',
+              payload: 'Payload for first bubble',
             }],
           }, {
-            title: "touch",
-            subtitle: "Your Hands, Now in VR",
-            item_url: "https://www.oculus.com/en-us/touch/",
-            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+            title: 'touch',
+            subtitle: 'Your Hands, Now in VR',
+            item_url: 'https://www.oculus.com/en-us/touch/',
+            image_url: 'http://messengerdemo.parseapp.com/img/touch.png',
             buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/touch/",
-              title: "Open Web URL"
+              type: 'web_url',
+              url: 'https://www.oculus.com/en-us/touch/',
+              title: 'Open Web URL',
             }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for second bubble",
-            }]
-          }]
-        }
-      }
-    }
+              type: 'postback',
+              title: 'Call Postback',
+              payload: 'Payload for second bubble',
+            }],
+          }],
+        },
+      },
+    },
   };
   exports.callSendAPI(messageData);
-}
+};
 
 
 /** This function demonstrates the Quick Reply capability which provides the users buttons to respond and returns a defined payload */
 // Reference: https://developers.facebook.com/docs/messenger-platform/send-api-reference/quick-replies
-exports.sendQuickReply = function (recipientId) {
+exports.sendQuickReply = function(recipientId, question1) {
   console.log('\nWe heard \'quick reply\', get the Quick Reply template!');
-  var messageData = {
+  let messageData = {
     recipient: {
-      id: recipientId
+      id: recipientId,
     },
     message: {
-      text: "Pick a color:",
+      text: 'Pick a color:',
       quick_replies: [
         {
-          content_type: "text",
-          title: "Red",
-          payload: "answered_q1", // Recieves this payload back as the new 'message, so maybe just grab this and toss it into FB as part of the 'incoming' loop?
+          content_type: 'text',
+          title: 'Red',
+          payload: 'answered_q1', // Recieves this payload back as the new 'message, so maybe just grab this and toss it into FB as part of the 'incoming' loop?
           // TODO: Maybe explore Postbacks for Quick Replies, if necesssary
-          image_url: "http://petersfantastichats.com/img/red.png" // Even takes a cute little image for friendliness
+          image_url: 'http://petersfantastichats.com/img/red.png', // Even takes a cute little image for friendliness
           // TODO: Emojis?!
         },
         {
-          content_type: "text",
-          title: "Green",
-          payload: "answered_q1",
-          image_url: "http://petersfantastichats.com/img/green.png"
-        }
-      ]
-    }
+          content_type: 'text',
+          title: 'Green',
+          payload: 'answered_q1',
+          image_url: 'http://petersfantastichats.com/img/green.png',
+        },
+      ],
+    },
   };
   exports.callSendAPI(messageData);
-}
+};
+
+exports.sendQuickHello = function(recipientId) {
+  let messageData = {
+    recipient: {
+      id: recipientId,
+    },
+    message: {
+      text: 'Hello, I\'m Opi! Ready to get started?',
+      quick_replies: [
+        {
+          content_type: 'text',
+          title: '😊',
+          payload: 'answered_q1', // Recieves this payload back as the new 'message, so maybe just grab this and toss it into FB as part of the 'incoming' loop?
+          // TODO: Maybe explore Postbacks for Quick Replies, if necesssary
+          image_url: 'http://petersfantastichats.com/img/red.png', // Even takes a cute little image for friendliness
+          // TODO: Emojis?!
+        },
+        {
+          content_type: 'text',
+          title: 'Green',
+          payload: 'answered_q1',
+          image_url: 'http://petersfantastichats.com/img/green.png',
+        },
+      ],
+    },
+  };
+  exports.callSendAPI(messageData);
+};
 
 
