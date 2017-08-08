dc3d8b512c4bb25db436dfe0744758ff7df36517
Vivek Menon <mail@vvkmnn.xyz>
2017-08-08 14:29:49 -0400

    Successfully deployed a Firebase Cloud Function!

--- bot-functions/index.js
+++ bot-functions/index.js
@@ -1,19 +1,36 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const request = require('request');
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!');
-});
+// Intialize Firebase
+admin.initializeApp(functions.config().firebase);
 
-exports.bigben = functions.https.onRequest((req, res) => {
-  const hours = (new Date().getHours() % 12) + 1; // london is UTC + 1hr;
-  res.status(200).send(`<!doctype html>
-      <head>
-        <title>Time</title>
-      </head>
-      <body>
-        ${'BONG '.repeat(hours)}
-      </body>
-    </html>`);
-});
+/** 
+ * Gets User Data and update our entry in the DB
+ */
+exports.getUserData = functions.database.ref('users/{userID}')
+  .onCreate((event) => {
+    const userID = event.params.userID;
+    const pageID = 'EAAavks7uF3cBADXbtZAo4BC3nbZCL6IfMHsCPZBn9ZBRQyu9r2KbOkN8YUdclDLHf2QHAw5ZApUl1C9ymhmi9ooRsYWlBySrK0unwizMZAhpVi7NgcMMgl9CsVP2Q8qAsOjMSyosvn0JvLwlpaYZAohb3OM4Xf67yTIHtRcAzUzAQZDZD';
+
+    request
+      .get({
+        url: 'https://graph.facebook.com/v2.6/' + userID + '?fields=first_name,last_name,locale,gender&access_token=' + pageID,
+        json: true,
+        headers: {'User-Agent': 'request'},
+      }, (err, res, data) => {
+        admin.database().ref('users/' + userID)
+          .update({
+            firstName: data.first_name,
+            lastName: data.last_name,
+            language: data.locale,
+            gender: data.gender,
+          })
+          .then(() => {
+            console.log('Successly queried Facebook, and update Database!');
+          });
+      })
+      .on('error', (err) => {
+        console.error('Error querying Facebook!');
+      });
+  });
