86dacdf31a8f66fe9bc2fab32fc92ed6121c6c0d
Vivek Menon <mail@vvkmnn.xyz>
2017-10-05 18:23:31 -0400

    (refactor): Renaming stuff. Let's start snipping.

--- bot/index.js
+++ bot/index.js
@@ -1,6 +1,12 @@
+'use strict';
+import message_handler from 'messenger';
+const barFunction = require('./bar');
+
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const request = require('request');
+let logger = require('winston');
+
 
 import {bot} from 'bot';
 // intialize firebase
