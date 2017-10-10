c4ad72287193ef8e57d42c87da6cd596e369f068
Yash Paliwal <y.paliwal92@gmail.com>
2017-10-10 00:04:13 -0400

    Client UI started in a Angular2 project

---
+++ web/src/app/ui/create-survey/create-survey.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'create-survey',
+  templateUrl: './create-survey.component.html',
+  styleUrls: ['./create-survey.component.scss']
+})
+export class CreateSurveyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
