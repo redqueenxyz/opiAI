06f8f2beb1a7cba1166acd02f72291a5f71f9331
Yash Paliwal <y.paliwal92@gmail.com>
2017-11-21 19:02:50 +0100

    added survey results' table

--- web/src/app/ui/survey-results/survey-results.component.ts
+++ web/src/app/ui/survey-results/survey-results.component.ts
@@ -13,17 +13,26 @@ import 'rxjs/add/operator/map';
 })
 export class SurveyResultsComponent implements OnInit {
 
-  public doughnutChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-  public doughnutChartData:number[] = [350, 450, 100];
-  public doughnutChartType:string = 'doughnut';
+  // GRAPH VARS
+  public chart_data;
+  public chart_labels;
+  public chartData;
 
+
+  // SURVEY VARS
   public all_surveys;
   public current_survey;
   public current_survey_name;
   public current_survey_questions;
 
+  public survey_responses = {};
+
   public selected_question;
   public question_details_modal_boolean: boolean = false;
+
+  // STATS
+  public num_of_responses;
+  public num_of_questions;
 
   constructor(private _authSrv: AuthService,
               private _router: Router,
@@ -34,43 +43,87 @@ export class SurveyResultsComponent implements OnInit {
     this._activeRoute.params.subscribe((params: Params) => {
       // Get current survey name from query params
       this.current_survey_name = params['survey_name'];
-      this.getSurveyResults(this.current_survey_name);
+      // this.getSurveyResults(this.current_survey_name);
       if(this._authSrv.all_surveys == null) {
         this._router.navigate(['dashboard']);
       } else {
         this.all_surveys = this._authSrv.all_surveys;
-        console.log(this.all_surveys);
+        // console.log(this.all_surveys);
         for(var i = 0; i < this.all_surveys.length; i++) {
           // FIND the current survey, and store locally in component
           if(this.all_surveys[i]['postback'] == this.current_survey_name) {
             this.current_survey = this.all_surveys[i];
             this.current_survey_questions = this.current_survey['questions'];
             // ADDING COUNTS of Quick replies to each question
+            this.num_of_questions = this.current_survey_questions.length;
             for(var j = 0; j < this.current_survey_questions.length; j++) {
               this.current_survey['questions'][j]['quick_replies_length'] = this.current_survey['questions'][j]['quick_replies'].length;
               this.current_survey['questions'][j]['question_index'] = j+1;
+              this.getSurveyResults(this.current_survey_name, j.toString());
             }
-            console.log(this.current_survey);
+            // console.log(this.current_survey);
           }
         }
       }
     })
   }
 
-  getSurveyResults(survey_name) {
-    console.log(survey_name);
-    const collection = this._afs.collection('surveys');
-    // const collection = this._afs.collection('responses').doc(survey_name).collection('users');
-    const collection$ = collection.valueChanges();
-    collection$.subscribe(data => {
-        console.log(data);
-    })
+  getSurveyResults(survey_name, question_number) {
+    const responses = this._afs
+      .collection("responses")
+      .doc(survey_name)
+      .collection("questionAnswered")
+      .doc(question_number)
+      .collection("respondents")
+      .valueChanges()
+      .subscribe(data => {
+        console.log("Survey: ", survey_name, ", question: ", question_number);
+        console.log('Data: ', data);
+
+        this.survey_responses[question_number] = data;
+
+        // if first question, do a count of the data arrary
+        if(question_number == '0') {
+          this.num_of_responses = data.length;
+        }
+      })
 
   }
 
   openQuestionDetailsModal(question) {
     console.log(question);
+    console.log(this.survey_responses);
+    var resp_arr = this.survey_responses[question['question_index'] - 1];
+    var resp_obj = {};
+    console.log(resp_arr);
 
+    for(var i = 0; i < resp_arr.length; i++) {
+      if(!resp_obj[resp_arr[i]['answer']]) {
+        resp_obj[resp_arr[i]['answer']] = [true];
+      } else {
+        resp_obj[resp_arr[i]['answer']].push(true);
+      }
+    }
+
+    for(var key in resp_obj) {
+      var daArr = resp_obj[key];
+      resp_obj[key] = daArr.length;
+    }
+
+    console.log(resp_obj);
+
+    var label_arr = Object.keys(resp_obj);
+    var result_arr = [];
+    for(var key in resp_obj) {
+      result_arr.push(resp_obj[key]);
+    }
+
+    this.chart_data = result_arr;
+    this.chart_labels = label_arr;
+
+    console.log(this.chart_data);
+    console.log(this.chart_labels);
+    this.chartData = [this.chart_data, this.chart_labels];
     this.question_details_modal_boolean = true;    
     this.selected_question = question;
   } 
